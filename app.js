39c34c41205bfdcfa8027e39aca5a27636776447
Nick Sophocleous <nicksoph@gmail.com>
2017-01-05 14:30:56 +0000

    new

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require('./api/data/db.js');
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 
+var routes = require('./api/routes');
 
 // Define the port to run on
 app.set('port', 3000);
@@ -15,22 +18,11 @@ app.use(function(req, res, next) {
 // Set static directory before defining routes
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Enable parsing of posted forms
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Add some routing
-
-app.get('/json', function(req, res) {
-  console.log("GET the json");
-  res
-    .status(200)
-    .json( {"jsonData" : true} );
-});
-
-app.get('/file', function(req, res) {
-  console.log("GET the file");
-  res
-    .status(200)
-    .sendFile(path.join(__dirname, 'app.js'));
-});
-
+app.use('/api', routes);
 
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
