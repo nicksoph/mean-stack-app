c9d879244d3795bca7274854237aba9ccf5fda06
Nick Sophocleous <nicksoph@gmail.com>
2017-01-05 23:54:41 +0000

    restructure

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,20 +7,18 @@ var ctrlReviews = require('../controllers/reviews.controllers.js');
 // Hotel routes
 router
   .route('/hotels')
-  .get(ctrlHotels.hotelsGetAll);
+  .get(ctrlHotels.hotelsGetAll)
+  .post(ctrlHotels.hotelsAddOne);
 
 router
   .route('/hotels/:hotelId')
   .get(ctrlHotels.hotelsGetOne);
 
-router
-  .route('/hotels/new')
-  .post(ctrlHotels.hotelsAddOne);
-
 // Review routes
 router
   .route('/hotels/:hotelId/reviews')
-  .get(ctrlReviews.reviewsGetAll);
+  .get(ctrlReviews.reviewsGetAll)
+  .post(ctrlReviews.reviewsAddOne);
 
 router
   .route('/hotels/:hotelId/reviews/:reviewId')
